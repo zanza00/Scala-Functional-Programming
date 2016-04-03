0dee2c800fe32f6fe53305d2bf7cf6220a5da8cb
Simone Picciani <zanza00@gmail.com>
2016-04-03 21:05:53 +0200

    formatting

--- src/exercises/chapter2/Fibonacci.scala
+++ src/exercises/chapter2/Fibonacci.scala
@@ -59,9 +59,9 @@ object MyModule {
     loop(0)
   }
 
-  def findFirst[A] (as:Array[A], p: A => Boolean) : Int = {
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
     @annotation.tailrec
-    def loop(n:Int): Int =
+    def loop(n: Int): Int =
       if (n >= as.length) -1
       else if (p(as(n))) n
       else loop(n + 1)
@@ -69,11 +69,11 @@ object MyModule {
     loop(0)
   }
 
-  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
     @annotation.tailrec
     def loop(n: Int): Boolean =
-      if (n >= as.length -1 ) false
-      else if (ordered(as(n + 1),as(n))) true
+      if (n >= as.length - 1) false
+      else if (ordered(as(n + 1), as(n))) true
       else loop(n + 1)
 
     loop(0)
