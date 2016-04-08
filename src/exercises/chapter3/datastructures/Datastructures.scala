96995f92a7a5748bb67957379513be003471ced5
Simone Picciani <zanza00@gmail.com>
2016-04-08 20:46:11 +0200

    first example

---
+++ src/exercises/chapter3/datastructures/Datastructures.scala
@@ -0,0 +1,28 @@
+package exercises.chapter3.datastructures
+
+/**
+  * Created by zanza on 04/04/2016.
+  */
+sealed trait List[+A]
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0 // empty list sum is zero
+    case Cons(x, xs) => x + sum(xs) //sum of first plus sum of the rest of the list
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+}
