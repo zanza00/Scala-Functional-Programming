77d3012d83379ed945d2f77b3b3d5910d0a5e73b
Simone Picciani <zanza00@gmail.com>
2016-04-09 21:24:20 +0200

    second and third exercise

--- src/exercises/chapter3/datastructures/Datastructures.scala
+++ src/exercises/chapter3/datastructures/Datastructures.scala
@@ -25,4 +25,21 @@ object List {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+
+  val x = List(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  }
+
+  def tail[A](l :List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_,t) => t
+  }
+
+
+
 }
+
