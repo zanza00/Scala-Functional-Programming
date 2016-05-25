e64c04dbc81035a702641a7ecb21d4bbecc6f983
Simone Picciani <zanza00@gmail.com>
2016-05-25 23:11:25 +0200

    formatting

--- src/exercises/chapter3/datastructures/Datastructures.scala
+++ src/exercises/chapter3/datastructures/Datastructures.scala
@@ -114,9 +114,9 @@ object List {
     foldRight(as, 0)((_, x) => x + 1)
 
   @annotation.tailrec
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B =
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B =
     l match {
       case Nil => z
-      case Cons(h,t) => foldLeft(t, f(z,h))(f)
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
     }
 }
